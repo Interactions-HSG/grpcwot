2fcade4c6de32e409237decd958eae00318abc9b
Stefan Eibl <stefan.eibl@student.unisg.ch>
2022-03-18 00:58:39 +0100

    feat: read and parse proto
    read an parse the previously added "sample.proto" file
    
    links #3

--- src/Main.go
+++ src/Main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/emicklei/proto"
+	"os"
 )
 
 var messages map[string]*proto.Message
@@ -10,5 +11,19 @@ var rpcFunctions []*proto.RPC
 func main() {
 	// initialize the messages
 	messages = make(map[string]*proto.Message)
+
+	// open sample proto file
+	reader, _ := os.Open("sample.proto")
+	// close reader after the execution finished
+	defer func(reader *os.File) {
+		err := reader.Close()
+		if err != nil {
+
+		}
+	}(reader)
+
+	// parse the sample.proto with the protoparser as described on emicklei/proto Github page
+	parser := proto.NewParser(reader)
+	definition, _ := parser.Parse()
 
 }

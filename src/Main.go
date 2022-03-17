bf24e3b8a900a8dfcb94185bdc92ae4d130efe27
Stefan Eibl <stefan.eibl@student.unisg.ch>
2022-03-18 00:57:42 +0100

    feat: declare variables
    declares and initializes the map and the slice to store Messages and RPC-Functions
    
    links #3

--- src/Main.go
+++ src/Main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/emicklei/proto"
+)
+
+var messages map[string]*proto.Message
+var rpcFunctions []*proto.RPC
+
 func main() {
+	// initialize the messages
+	messages = make(map[string]*proto.Message)
 
 }

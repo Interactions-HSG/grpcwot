81699baa9cb5cde3941ade36d9a5ab530ddc1e84
Stefan Eibl <stefan.eibl@student.unisg.ch>
2022-03-18 04:10:56 +0100

    feat: add getCommentLineContent
    add a function to extract a specific information out of a comment based on a specific annotation
    
    link #10

--- src/traverseLinkedRPC.go
+++ src/traverseLinkedRPC.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
 type AllowedClasses string
 
 // defines classes on which it is allowed to get fields
@@ -17,4 +23,28 @@ var allowedAccess = map[AllowedClasses][]string{
 	"Elements", "InlineComment", "Parent"*/
 	ProtoComment: {"Position", "Lines"}, /*Cstyle, ExtraSlash*/
 	ProtoMessage: {"Position", "Comment", "Name" /*, "IsExtend", "Elements", "Parent"*/},
+}
+
+/*
+@param commentTag: the tag to identify which line of a comment should be read
+@param commentLines: a reflect.Value which holds a []string with all lines of a comment
+This function searches for a line annotated with the comment Tag and returns the value in this line without the annotation
+*/
+func getCommentLineContent(commentTag string, commentLines reflect.Value) string {
+	commentTag = strings.Trim(commentTag, "<>")
+	var line string
+	for i := 0; i < commentLines.Len(); i++ {
+		line = commentLines.Index(i).String()
+		// Identifies if the line starts with the searched tag
+		if strings.HasPrefix(strings.TrimLeft(line, "\t \n"), "@"+commentTag) {
+			// returns the trimmed value of the annotated line without the annotation
+			return strings.Trim(
+				strings.TrimPrefix(
+					strings.TrimLeft(line, "\t \n"),
+					"@"+commentTag+":"),
+				"\t \n")
+		}
+	}
+	fmt.Println("no comment found for <", commentTag, ">")
+	return ""
 }

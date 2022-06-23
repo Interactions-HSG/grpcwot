d019abae230c6c83c89dd2be8c8380dde06cb78c
Stefan Eibl <77973550+S-Eibl@users.noreply.github.com>
2022-06-23 17:32:26 +0200

    refac(message-reference): introduce own data schema builder
    - moved message interpretation logic from builder to data_schema_builder together with the corresponding tests to data_schema_builder_test.go
    - Call generateDataSchema from GenerateTDfromProtoBuf

--- builder.go
+++ builder.go
@@ -2,34 +2,24 @@ package grpcwot
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
-	"strings"
 
-	"github.com/Interactions-HSG/grpcwot/pkg/protofmt"
 	"github.com/emicklei/proto"
 	"github.com/linksmart/thing-directory/wot"
 )
 
-type refMesTuple struct {
-	pm string // Parent message where the field of type message is included
-	t  string // Type of the field == name of the referenced message
-	n  string // Name of the field
-}
-
 type builder struct {
 	td   wot.ThingDescription
-	ds   map[string]*wot.DataSchema
+	dsb  *dataSchemaBuilder
 	ip   string
 	port int
-	lm   []refMesTuple
 }
 
-func newBuilder(ip string, port int) *builder {
+func newBuilder(ip string, port int, dsb *dataSchemaBuilder) *builder {
 	return &builder{
 		td:   wot.ThingDescription{},
-		ds:   map[string]*wot.DataSchema{},
+		dsb:  dsb,
 		ip:   ip,
 		port: port,
 	}
@@ -53,8 +43,8 @@ func (b *builder) HandleRPC(r *proto.RPC) {
 		b.td.Actions = map[string]wot.ActionAffordance{}
 	}
 	affordance := wot.ActionAffordance{}
-	affordance.Input = *b.ds[r.RequestType]
-	affordance.Output = *b.ds[r.ReturnsType]
+	affordance.Input = *b.dsb.ds[r.RequestType]
+	affordance.Output = *b.dsb.ds[r.ReturnsType]
 	affordance.Forms = []wot.Form{
 		{
 			Href:        b.GetIRI(r.Name),
@@ -66,151 +56,8 @@ func (b *builder) HandleRPC(r *proto.RPC) {
 	b.td.Actions[r.Name] = affordance
 }
 
-// getFullMessageName returns the complete Message name from the root level to the actual message
-func getFullMessageName(m *proto.Message) string {
-	if v, ok := m.Parent.(*proto.Message); ok {
-		return getFullMessageName(v) + "." + m.Name
-	} else {
-		return m.Name
-	}
-}
-
-// HandleMessage build a DataSchema: https://www.w3.org/TR/wot-thing-description/#dataschema
-// from a Message in the protobuf definition
-func (b *builder) HandleMessage(m *proto.Message) {
-	fullMessageName := getFullMessageName(m)
-	if _, ok := b.ds[fullMessageName]; !ok {
-		b.ds[fullMessageName] = &wot.DataSchema{
-			DataType: "object",
-			ObjectSchema: &wot.ObjectSchema{
-				Properties: map[string]wot.DataSchema{},
-			},
-		}
-	}
-	for _, v := range m.Elements {
-		switch protofmt.NameOfVisitee(v) {
-		case "NormalField":
-			b.ds[fullMessageName].ObjectSchema.Properties[v.(*proto.NormalField).Field.Name] =
-				b.fieldToDataSchema(v.(*proto.NormalField).Field, fullMessageName)
-		case "Comment":
-		case "Oneof":
-			b.ds[fullMessageName].ObjectSchema.Properties[v.(*proto.Oneof).Name] =
-				wot.DataSchema{OneOf: b.oneofToDataSchema(v.(*proto.Oneof), fullMessageName)}
-		}
-	}
-}
-
-// fieldToDataSchema converts the given proto's message field into a WoT DataScheme
-// cf. https://www.w3.org/TR/wot-thing-description/#dataschema
-func (b *builder) fieldToDataSchema(f *proto.Field, messageName string) wot.DataSchema {
-	var fieldType string
-	switch f.Type {
-	case "double", "float":
-		fieldType = "number"
-	case "int32", "int64", "uint32", "uint64", "sint32", "sint64", "fixed32", "fixed64", "sfixed32", "sfixed64":
-		fieldType = "integer"
-	case "bool":
-		fieldType = "boolean"
-	case "string", "bytes":
-		fieldType = "string"
-	default:
-		fieldType = "object"
-		b.lm = append(b.lm, refMesTuple{pm: messageName, t: f.Type, n: f.Name})
-	}
-	return wot.DataSchema{DataType: fieldType}
-}
-
-func (b *builder) oneofToDataSchema(oo *proto.Oneof, messageName string) []wot.DataSchema {
-	oof := []wot.DataSchema{}
-	for _, v := range oo.Elements {
-		oof = append(oof, b.fieldToDataSchema(v.(*proto.OneOfField).Field, messageName))
-	}
-	return oof
-}
-
-func (b *builder) resolveSingleReference(elem refMesTuple) (string, error) {
-	parts := strings.Split(elem.pm, ".")
-	for k := len(parts); k >= 0; k-- {
-		s := strings.Join(parts[:k], ".") + "." + elem.t
-		if k == 0 {
-			s = elem.t
-		}
-		if _, ok := b.ds[s]; ok {
-			return s, nil
-		}
-	}
-	return "", errors.New("No corresponding message found for type reference " + elem.t +
-		" in message " + elem.pm)
-}
-
-// Resolves all references in lm by calling resolveSingleReference on them
-// Sets the t (type) property of each refMesTuple to the full message name of the referenced message
-func (b *builder) resolveAllReferences() error {
-	for k, v := range b.lm {
-		res, err := b.resolveSingleReference(v)
-		if err != nil {
-			return err
-		}
-		v.t = res
-		b.lm[k] = v
-	}
-	return nil
-}
-
-// containsCircle determines if the message references hold a circle and would lead to infinite injection
-// For the search algorithm DFS would be an alternative with a better worst case performance
-// but the structure of lm would have to be modified and for proto file's messages the average use case does not draw
-// on long paths through the graph (the strengths of DFS)
-func containsCircle(lm []refMesTuple) error {
-	ended := make([]bool, len(lm))
-	count := len(lm)
-	last := count
-	for count != 0 {
-	out:
-		for k, v := range lm {
-			if ended[k] {
-				continue
-			}
-			for k2, v2 := range lm {
-				if ended[k2] {
-					continue
-				}
-				if v.t == v2.pm {
-					continue out
-				}
-			}
-			ended[k] = true
-			count--
-		}
-		if last == count {
-			return errors.New("proto file contained circle reference in the Messages")
-		}
-	}
-	return nil
-}
-
-// Extend the data schemas with references to other data schemas representing the message references
-func (b *builder) constructMessagesNested() error {
-	err := b.resolveAllReferences()
-	if err != nil {
-		return err
-	}
-
-	err = containsCircle(b.lm)
-	if err != nil {
-		return err
-	}
-
-	for _, v := range b.lm {
-		b.ds[v.pm].ObjectSchema.Properties[v.n] = *b.ds[v.t]
-	}
-	return nil
-}
-
 // GenerateTDfromProtoBuf parses `protoFile` to generate `tdFile`
 func GenerateTDfromProtoBuf(protoFile, tdFile, ip string, port int) error {
-	// initialize the TD builder with an empty TD and DataSchema
-	b := newBuilder(ip, port)
 
 	// parse the protoFile with the emicklei/proto
 	reader, _ := os.Open(protoFile)
@@ -221,13 +68,18 @@ func GenerateTDfromProtoBuf(protoFile, tdFile, ip string, port int) error {
 		return nil
 	}
 
-	// extract the Messages as DataSchema
-	proto.Walk(definition,
-		proto.WithService(b.HandleService),
-		proto.WithMessage(b.HandleMessage))
+	// Read the Messages and produce DataSchemes
+	dsb, err := generateDataSchemas(definition)
+	if err != nil {
+		return err
+	}
+
+	// initialize the TD builder with an empty TD and DataSchema
+	b := newBuilder(ip, port, dsb)
 
 	// translate the RPC functions into Interaction Affordances
 	proto.Walk(definition,
+		proto.WithService(b.HandleService),
 		proto.WithRPC(b.HandleRPC))
 
 	// serialize the TD to JSONLD
---
+++ data_schema_builder_test.go
@@ -0,0 +1,529 @@
+package grpcwot
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/emicklei/proto"
+	"github.com/linksmart/thing-directory/wot"
+)
+
+// Message parsing test for scalar value fields in fieldToDataSchema()
+// Every scalar value type listed for proto3 (https://developers.google.com/protocol-buffers/docs/proto3#scalar) is included with one field
+// The test expects, that all scalar value types are converted into the corresponding json value type according to the table provided above
+var scalarValueFieldsTests = []struct {
+	in  *proto.Field
+	out wot.DataSchema
+}{
+	{
+		&proto.Field{Type: "double"},
+		wot.DataSchema{DataType: "number"},
+	},
+	{
+		&proto.Field{Type: "float"},
+		wot.DataSchema{DataType: "number"},
+	},
+	{
+		&proto.Field{Type: "int32"},
+		wot.DataSchema{DataType: "integer"},
+	},
+	{
+		&proto.Field{Type: "int64"},
+		wot.DataSchema{DataType: "integer"},
+	},
+	{
+		&proto.Field{Type: "uint32"},
+		wot.DataSchema{DataType: "integer"},
+	},
+	{
+		&proto.Field{Type: "uint64"},
+		wot.DataSchema{DataType: "integer"},
+	},
+	{
+		&proto.Field{Type: "sint32"},
+		wot.DataSchema{DataType: "integer"},
+	},
+	{
+		&proto.Field{Type: "sint64"},
+		wot.DataSchema{DataType: "integer"},
+	},
+	{
+		&proto.Field{Type: "fixed32"},
+		wot.DataSchema{DataType: "integer"},
+	},
+	{
+		&proto.Field{Type: "fixed64"},
+		wot.DataSchema{DataType: "integer"},
+	},
+	{
+		&proto.Field{Type: "sfixed32"},
+		wot.DataSchema{DataType: "integer"},
+	},
+	{
+		&proto.Field{Type: "sfixed64"},
+		wot.DataSchema{DataType: "integer"},
+	},
+	{
+		&proto.Field{Type: "bool"},
+		wot.DataSchema{DataType: "boolean"},
+	},
+	{
+		&proto.Field{Type: "string"},
+		wot.DataSchema{DataType: "string"},
+	},
+	{
+		&proto.Field{Type: "bytes"},
+		wot.DataSchema{DataType: "string"},
+	},
+}
+
+func TestScalarFields(t *testing.T) {
+	b := newDataSchemaBuilder()
+	for _, tt := range scalarValueFieldsTests {
+		result := b.fieldToDataSchema(tt.in, "")
+		if result.DataType != tt.out.DataType {
+			t.Errorf("fieldToDataSchema(%v) => \n%v, want \n%v", tt.in, result, tt.out)
+		}
+	}
+}
+
+var messageNameDeterminationTest = []struct {
+	in  *proto.Message
+	out string
+}{
+	{
+		&proto.Message{Name: "Test"},
+		"Test",
+	},
+	{
+		&proto.Message{Name: "Test", Parent: &proto.Message{Name: "ParentTest"}},
+		"ParentTest.Test",
+	},
+	{
+		&proto.Message{Name: "Test",
+			Parent: &proto.Message{Name: "ParentTest",
+				Parent: &proto.Message{Name: "ParentParentTest"}}},
+		"ParentParentTest.ParentTest.Test",
+	},
+	{
+		&proto.Message{Name: "Test",
+			Parent: &proto.Service{Name: "Service"}},
+		"Test",
+	},
+}
+
+func TestGetFullMessageName(t *testing.T) {
+	for _, tt := range messageNameDeterminationTest {
+		result := getFullMessageName(tt.in)
+		if result != tt.out {
+			t.Errorf("getFullMessageName(%v) => \n%v, want \n%v", tt.in, result, tt.out)
+		}
+	}
+}
+
+func errorCheck(t *testing.T, exp error, act error) {
+	if act == nil && exp != nil {
+		t.Errorf("Expected error %v,\n but no error was raised", exp.Error())
+	} else if act != nil && exp == nil {
+		t.Errorf("Expected no error, but the follwing error was raised:\n%v", act.Error())
+	} else if act != nil && exp != nil && act.Error() != exp.Error() {
+		t.Errorf("Expected error message: %v\nbut got: %v", exp.Error(), act.Error())
+	}
+}
+
+var resolveSingleReferenceTest = []struct {
+	in     dataSchemaBuilder
+	inElem refMesTuple
+	out    string
+	err    error
+}{
+	{
+		// message ParentMessage {
+		//   ReferencedMessage testField = 1;
+		// }
+		// message ReferencedMessage {}
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"ParentMessage":     {},
+				"ReferencedMessage": {},
+			},
+		},
+		refMesTuple{pm: "ParentMessage", t: "ReferencedMessage", n: "testField"},
+		"ReferencedMessage",
+		nil,
+	},
+	{
+		// message Message1 {
+		//   message Message2 {
+		//     message Message3 {}
+		//     Message3 testField = 1;
+		//   }
+		//   Message2 testField = 1;
+		// }
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1":                   {},
+				"Message1.Message2":          {},
+				"Message1.Message2.Message3": {},
+			},
+		},
+		refMesTuple{pm: "Message1", t: "Message2", n: "testField"},
+		"Message1.Message2",
+		nil,
+	},
+	{
+		// message Message1 {
+		//   message Message2 {
+		//     message Message3 {}
+		//     Message3 testField = 1;
+		//   }
+		//   Message2 testField = 1;
+		// }
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1":                   {},
+				"Message1.Message2":          {},
+				"Message1.Message2.Message3": {},
+			},
+		},
+		refMesTuple{pm: "Message1.Message2", t: "Message3", n: "testField"},
+		"Message1.Message2.Message3",
+		nil,
+	},
+	{
+		// message Message1 {
+		//   message Message2 {
+		//     message Message3 {
+		//       message Message2 {}
+		//       Message2.Message3.Message2 testField = 1;
+		//       Message5 testField1 = 2;
+		//     }
+		//     Message3.Message2 testField = 1;
+		//   }
+		//   Message2.Message3 testField = 1;
+		// }
+		// message Message5 {}
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1":                            {},
+				"Message1.Message2":                   {},
+				"Message1.Message2.Message3":          {},
+				"Message1.Message2.Message3.Message2": {},
+				"Message5":                            {},
+			},
+		},
+		refMesTuple{pm: "Message1.Message2.Message3", t: "Message5", n: "testField"},
+		"Message5",
+		nil,
+	},
+	{
+		// message Message1 {
+		//   message Message2 {
+		//     message Message3 {
+		//       message Message2 {}
+		//       Message2.Message3.Message2 testField = 1;
+		//       Message5 testField1 = 2;
+		//     }
+		//     Message3.Message2 testField = 1;
+		//   }
+		//   Message2.Message3 testField = 1;
+		// }
+		// message Message5 {}
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1":                            {},
+				"Message1.Message2":                   {},
+				"Message1.Message2.Message3":          {},
+				"Message1.Message2.Message3.Message2": {},
+				"Message5":                            {},
+			},
+		},
+		refMesTuple{pm: "Message1.Message2.Message3", t: "Message2.Message3.Message2", n: "testField"},
+		"Message1.Message2.Message3.Message2",
+		nil,
+	},
+	{
+		// message Message1 {
+		//   message Message2 {
+		//     message Message3 {
+		//       message Message2 {}
+		//       Message2.Message3.Message2 testField = 1;
+		//       Message5 testField1 = 2;
+		//     }
+		//     Message3.Message2 testField = 1;
+		//   }
+		//   Message2.Message3 testField = 1;
+		// }
+		// message Message5 {}
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1":                            {},
+				"Message1.Message2":                   {},
+				"Message1.Message2.Message3":          {},
+				"Message1.Message2.Message3.Message2": {},
+				"Message5":                            {},
+			},
+		},
+		refMesTuple{pm: "Message1", t: "Message2.Message3", n: "testField"},
+		"Message1.Message2.Message3",
+		nil,
+	},
+	{
+		// message Message1 {
+		//   Message2 testField = 1;
+		// }
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1": {},
+			},
+		},
+		refMesTuple{pm: "Message1", t: "Message2", n: "testField"},
+		"",
+		errors.New("No corresponding message found for type reference Message2 in message Message1"),
+	},
+	{
+		// message Message1 {
+		//   message Message2 {
+		//     message Message3 {}
+		//   }
+		//   message Message4 {
+		//     Message3 testField = 1;
+		//   }
+		// }
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1":                   {},
+				"Message1.Message2":          {},
+				"Message1.Message2.Message3": {},
+				"Message1.Message4":          {},
+			},
+		},
+		refMesTuple{pm: "Message1.Message4", t: "Message3", n: "testField"},
+		"",
+		errors.New("No corresponding message found for type reference Message3 in message Message1.Message4"),
+	},
+}
+
+func TestResolveSingleReference(t *testing.T) {
+	for _, tt := range resolveSingleReferenceTest {
+		result, err := tt.in.resolveSingleReference(tt.inElem)
+
+		errorCheck(t, tt.err, err)
+
+		if result != tt.out {
+			t.Errorf("Expected %v,\nbut got %v", tt.out, result)
+		}
+	}
+}
+
+type getDataSchemaFunc func(schema map[string]*wot.DataSchema) wot.DataSchema
+
+func getDataSchema(dsInsertMessage string, dsInsertFields []string) getDataSchemaFunc {
+	return func(schema map[string]*wot.DataSchema) wot.DataSchema {
+		ds := *schema[dsInsertMessage]
+		for _, v := range dsInsertFields {
+			ds = ds.ObjectSchema.Properties[v]
+		}
+		return ds
+	}
+}
+func getExpectedSchema(dsExpectedInsertedMessage string) getDataSchemaFunc {
+	return getDataSchema(dsExpectedInsertedMessage, []string{})
+}
+
+func createInitialMessageDataSchema(props []string) *wot.DataSchema {
+	ds := &wot.DataSchema{
+		DataType: "object",
+		ObjectSchema: &wot.ObjectSchema{
+			Properties: map[string]wot.DataSchema{},
+		},
+	}
+	for _, v := range props {
+		ds.ObjectSchema.Properties[v] = wot.DataSchema{
+			DataType: "object",
+		}
+	}
+	return ds
+}
+func createInitialMessageDataSchemaOneProperty(prop string) *wot.DataSchema {
+	return createInitialMessageDataSchema([]string{prop})
+}
+func createInitialMessageDataSchemaEmpty() *wot.DataSchema {
+	return createInitialMessageDataSchema([]string{})
+}
+
+type outRef struct {
+	getResultDataSchema   getDataSchemaFunc
+	getExpectedDataSchema getDataSchemaFunc
+}
+
+func getOutRef(dsInsertMessage string, dsInsertFields []string, dsInsertedMessage string) outRef {
+	return outRef{
+		getDataSchema(dsInsertMessage, dsInsertFields),
+		getExpectedSchema(dsInsertedMessage),
+	}
+}
+
+var constructMessageNested = []struct {
+	in  dataSchemaBuilder
+	out []outRef
+	err error
+}{
+	{
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1": createInitialMessageDataSchemaOneProperty("testField"),
+				"Message2": createInitialMessageDataSchemaEmpty(),
+			},
+			lm: []refMesTuple{
+				{pm: "Message1", t: "Message2", n: "testField"},
+			},
+		},
+		[]outRef{
+			getOutRef("Message1", []string{"testField"}, "Message2"),
+		},
+		nil,
+	},
+	{
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1": createInitialMessageDataSchemaOneProperty("testField"),
+				"Message2": createInitialMessageDataSchemaOneProperty("testField"),
+				"Message3": createInitialMessageDataSchemaEmpty(),
+			},
+			lm: []refMesTuple{
+				{pm: "Message1", t: "Message2", n: "testField"},
+				{pm: "Message2", t: "Message3", n: "testField"},
+			},
+		},
+		[]outRef{
+			getOutRef("Message1", []string{"testField"}, "Message2"),
+			getOutRef("Message2", []string{"testField"}, "Message3"),
+			getOutRef("Message1", []string{"testField", "testField"}, "Message3"),
+		},
+		nil,
+	},
+	{
+		//	message Message1 {
+		//	  message Message2 {
+		//	    message Message3 {
+		//	      message Message2 {}
+		//	      Message3 testField = 1;
+		//	      Message5 testField1 = 2;
+		//      }
+		//	    Message3.Message2 testField = 1;
+		//    }
+		//	  Message2.Message3 testField = 1;
+		//  }
+		//	message Message5 {}
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1":                            createInitialMessageDataSchemaOneProperty("testField"),
+				"Message1.Message2":                   createInitialMessageDataSchemaOneProperty("testField"),
+				"Message1.Message2.Message3":          createInitialMessageDataSchema([]string{"testField", "testField1"}),
+				"Message1.Message2.Message3.Message2": createInitialMessageDataSchemaEmpty(),
+				"Message5":                            createInitialMessageDataSchemaEmpty(),
+			},
+			lm: []refMesTuple{
+				{pm: "Message1", t: "Message2.Message3", n: "testField"},
+				{pm: "Message1.Message2", t: "Message3.Message2", n: "testField"},
+				{pm: "Message1.Message2.Message3", t: "Message1.Message2", n: "testField"},
+				{pm: "Message1.Message2.Message3", t: "Message5", n: "testField1"},
+			},
+		},
+		[]outRef{
+			// Message1 -> Message1.Message2.Message3
+			getOutRef("Message1", []string{"testField"}, "Message1.Message2.Message3"),
+			// Message1.Message2 -> Message1.Message2.Message3.Message2
+			getOutRef("Message1.Message2", []string{"testField"}, "Message1.Message2.Message3.Message2"),
+			// Message1.Message2.Message3 -> Message1.Message2
+			getOutRef("Message1.Message2.Message3", []string{"testField"}, "Message1.Message2"),
+			// Message1.Message2.Message3 -> Message5
+			getOutRef("Message1.Message2.Message3", []string{"testField1"}, "Message5"),
+			// Message1 -> Message1.Message2.Message3 -> Message5
+			getOutRef("Message1", []string{"testField", "testField1"}, "Message5"),
+			// Message1 -> Message1.Message2.Message3 -> Message1.Message2
+			getOutRef("Message1", []string{"testField", "testField"}, "Message1.Message2"),
+			// Message1.Message2.Message3 -> Message1.Message2 -> Message1.Message2.Message3.Message2
+			getOutRef("Message1.Message2.Message3", []string{"testField", "testField"},
+				"Message1.Message2.Message3.Message2"),
+			// Message1 -> Message1.Message2.Message3 -> Message1.Message2 -> Message1.Message2.Message3.Message2
+			getOutRef("Message1", []string{"testField", "testField", "testField"},
+				"Message1.Message2.Message3.Message2"),
+		},
+		nil,
+	},
+	{
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1": createInitialMessageDataSchemaOneProperty("testField"),
+				"Message2": createInitialMessageDataSchemaOneProperty("testField"),
+			},
+			lm: []refMesTuple{
+				{pm: "Message1", t: "Message2", n: "testField"},
+				{pm: "Message2", t: "Message1", n: "testField"},
+			},
+		},
+		[]outRef{},
+		errors.New("proto file contained circle reference in the Messages"),
+	},
+	{
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1": createInitialMessageDataSchemaOneProperty("testField"),
+			},
+			lm: []refMesTuple{
+				{pm: "Message1", t: "Message2", n: "testField"},
+			},
+		},
+		[]outRef{},
+		errors.New("No corresponding message found for type reference Message2 in message Message1"),
+	},
+	{
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1":                   createInitialMessageDataSchemaOneProperty("testField"),
+				"Message1.Message2":          createInitialMessageDataSchemaEmpty(),
+				"Message1.Message2.Message3": createInitialMessageDataSchemaEmpty(),
+			},
+			lm: []refMesTuple{
+				{pm: "Message1", t: "Message3", n: "testField"},
+			},
+		},
+		[]outRef{},
+		errors.New("No corresponding message found for type reference Message3 in message Message1"),
+	},
+	{
+		dataSchemaBuilder{
+			ds: map[string]*wot.DataSchema{
+				"Message1":          createInitialMessageDataSchemaOneProperty("testField"),
+				"Message1.Message2": createInitialMessageDataSchemaEmpty(),
+				"Message3":          createInitialMessageDataSchemaEmpty(),
+				"Message3.Message4": createInitialMessageDataSchemaEmpty(),
+			},
+			lm: []refMesTuple{
+				{pm: "Message1", t: "Message4", n: "testField"},
+			},
+		},
+		[]outRef{},
+		errors.New("No corresponding message found for type reference Message4 in message Message1"),
+	},
+}
+
+func TestConstructMessageNested(t *testing.T) {
+	for _, tt := range constructMessageNested {
+		err := tt.in.constructMessagesNested()
+
+		errorCheck(t, tt.err, err)
+
+		for _, out := range tt.out {
+			result := out.getResultDataSchema(tt.in.ds)
+			expected := out.getExpectedDataSchema(tt.in.ds)
+			if result.DataType != expected.DataType ||
+				result.ObjectSchema != expected.ObjectSchema {
+				t.Errorf("constructMessageNested() \n Expected inserted DataSchema \n%v \n but got DataSchema \n%v",
+					expected, result)
+			}
+		}
+	}
+}
